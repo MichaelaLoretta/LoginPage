f01dd64b426d208e8118fcb1e0ff89435b529cc7
Michaela <itsonlyrnr@gmail.com>
2022-10-13 09:40:59 +0200

    Initial Commit

---
+++ LoginPageSwiftUI/LoginPageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageSwiftUIApp.swift
+//  LoginPageSwiftUI
+//
+//  Created by Michaela Beyer on 2022-10-13.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
